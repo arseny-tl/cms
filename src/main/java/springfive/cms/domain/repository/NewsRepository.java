5ff53333cb92bc44f39a1d5f7af85beb1f98a47a
arseny-tl <wertynsky@gmail.com>
2018-09-20 07:04:18 +0300

    Fix some mistake

--- src/main/java/springfive/cms/domain/repository/NewsRepository.java
+++ src/main/java/springfive/cms/domain/repository/NewsRepository.java
@@ -1,5 +1,7 @@
 package springfive.cms.domain.repository;
 
+import org.springframework.stereotype.Service;
 import springfive.cms.domain.models.News;
 
+@Service
 public class NewsRepository extends AbstractRepository<News> {}

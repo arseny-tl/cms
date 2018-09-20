d3173391844974edb68d77bbda2787f4bfc8c6aa
arseny-tl <wertynsky@gmail.com>
2018-09-20 04:27:04 +0300

    Add News API *Also add NewsRequest *Also refactor imports

--- src/main/java/springfive/cms/domain/resources/UserResource.java
+++ src/main/java/springfive/cms/domain/resources/UserResource.java
@@ -3,7 +3,14 @@ package springfive.cms.domain.resources;
 import io.swagger.annotations.Api;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 import springfive.cms.domain.models.User;
 import springfive.cms.domain.vo.UserRequest;
 
